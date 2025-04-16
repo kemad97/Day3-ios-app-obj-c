d4d3b93896bfc484a68a21fd50d561937997d7ac
kemad97 <kerolos.emad992@gmail.com>
2025-04-16 09:08:59 +0200

    lab1 d

--- Day3 ios app/ViewController.m
+++ Day3 ios app/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import "ProfileViewController.h"
 
 @interface ViewController ()
 
@@ -18,5 +19,9 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)btnProfile:(id)sender {
+    ProfileViewController *profileVC = [self.storyboard instantiateViewControllerWithIdentifier:@"profID"];
+    [self.navigationController pushViewController:profileVC animated:YES];
+}
 
 @end
